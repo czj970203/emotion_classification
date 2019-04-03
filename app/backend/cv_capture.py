663ee6cb6a8e6565630f661996ee2adda6040dc9
Yuiheart <151250023@smail.nju.edu.cn>
2019-04-03 20:13:26 +0800

    4/3

---
+++ app/backend/cv_capture.py
@@ -0,0 +1,71 @@
+import cv2
+import os
+import numpy as np
+from keras.models import load_model
+import matplotlib.pyplot as plt
+
+def preprocess(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(basedir, 'haarcascade_frontalface_alt2.xml')
+    cap = cv2.CascadeClassifier(file_path)
+    faceRects = cap.detectMultiScale(
+        gray, scaleFactor=1.2, minNeighbors=3, minSize=(50, 50))
+    return gray, faceRects
+
+
+# 图片识别方法封装
+def discern(img):
+    gray, faceRects = preprocess(img)
+    if len(faceRects):
+        for faceRect in faceRects:
+            x, y, w, h = faceRect
+            cv2.rectangle(img, (x, y), (x + h, y + w), (0, 255, 0), 2)  # 框出人脸
+    return img
+
+
+def classify(img):
+    gray, faceRects = preprocess(img)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(basedir, 'simple_CNN.530-0.65.hdf5')
+    color = (255, 0, 0)
+    emotion_classifier = load_model(file_path)
+    for(x, y, w, h) in faceRects:
+        gray_face = gray[(y):(y + h), (x):(x + w)]
+        gray_face = cv2.resize(gray_face, (48, 48))
+        gray_face = gray_face / 255.0
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+        custom = emotion_classifier.predict(gray_face)
+        emotion_analysis(custom[0])
+
+
+def emotion_analysis(emotions):
+    objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    y_pos = np.arange(len(objects))
+    plt.bar(y_pos, emotions, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('percentage')
+    plt.title('emotion')
+    root = os.getcwd()
+    save_path = os.path.join(root, 'app/static/barchart.jpg')
+    plt.savefig(save_path)
+
+
+
+'''
+# 获取摄像头0表示第一个摄像头
+cap = cv2.VideoCapture("/Users/chenzejun/desktop/HCI最终版.mp4")
+startTime = time.time()
+while (1):  # 逐帧显示
+    ret, img = cap.read()
+    nowTime = time.time()
+    cv2.imshow("Image", img)
+    diff = round(nowTime - startTime, 3) * 1000
+    if(diff % 1000 <= 200):
+        discern(img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()  # 释放摄像头
+cv2.destroyAllWindows()  # 释放窗口资源
+'''
--- app/routes_for_camera.py
+++ app/routes_for_camera.py
@@ -1,16 +1,22 @@
-from flask import render_template, Response
-from app.models import VideoCamera
-from app import app
-from backend import cv_capture
-import cv2
 import os
+
+import cv2
+from flask import render_template, Response
+from app import app
+from app.backend import cv_capture
+from app.models import VideoCamera
 
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 cap = VideoCamera()
+#原始图片
+image = ''
+is_stopped = False
 is_closed = False
+is_uploaded = False
+
 
 @app.route('/')
 @app.route('/index')
@@ -25,32 +31,54 @@ def gen(camera):
         # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        if is_closed == True:
+            global is_stopped, is_closed_
+            is_stopped = True
+            break
 
 
 @app.route('/video_feed')  # 这个地址返回视频流响应
 def video_feed():
-    return Response(gen(cap),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(cap), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/close_video')
 def close_video():
-    cap.__del__()
+    global is_stopped, is_closed
     is_closed = True
+    while not is_stopped:
+        pass
+    cap.__del__()
     return render_template('index.html', is_closed=is_closed)
 
 @app.route('/open_video')
 def open_video():
     cap.__init__()
+    global is_stopped, is_closed
+    is_stopped = False
     is_closed = False
     return render_template('index.html', is_closed=is_closed)
 
 @app.route('/analysis', methods=['GET', 'POST'])
 def analysis():
     ret, img = cap.read()
+    global image
+    image = img
     img = cv_capture.discern(img)
     basedir = os.path.abspath(os.path.dirname(__file__))
     write_path = os.path.join(basedir, 'static/cached_images.jpg')
     address = 'static/cached_images.jpg'
     cv2.imwrite(write_path, img)
-    return render_template('index.html', address=address)
+    global is_uploaded
+    is_uploaded = True
+    return render_template('index.html', address=address, is_uploaded=is_uploaded)
+
+
+@app.route('/gen_bar')
+def gen_bar():
+    cv_capture.classify(image)
+    bar_addr = 'static/barchart.jpg'
+    global is_uploaded
+    is_uploaded=False
+    return render_template('index.html', bar_addr=bar_addr, is_uploaded=is_uploaded)
+
 
