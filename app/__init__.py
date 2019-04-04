de54d8d08974339f5ddd2761869653f1add6aa07
Yuiheart <151250023@smail.nju.edu.cn>
2019-04-04 14:44:21 +0800

    手动截屏版完成

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from datetime import timedelta
+
 
 app = Flask(__name__)
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+
 from app import routes_for_camera
+from app import models
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import cv2
+import os
 
+import tensorflow as tf
 
 class VideoCamera(object):
     def __init__(self):
@@ -18,3 +20,10 @@ class VideoCamera(object):
     def read(self):
         ret, image = self.video.read()
         return ret, image
+
+
+class EmotionClassifier(object):
+    def __init__(self):
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        model_path = os.path.join(basedir, 'backend/simple_CNN.530-0.65.hdf5')
+        self.emotion_classifier = tf.keras.models.load_model(model_path)
