57b309cf7c1910a2483dd3775ded7d1f18ddc37e
TrouBleMaKerV <782024062@qq.com>
2019-04-05 17:53:59 +0800

    4.5

--- app/routes_for_video.py
+++ app/routes_for_video.py
@@ -4,16 +4,14 @@ import cv2
 from flask import render_template, Response
 from app import app
 from app.backend import cv_capture
-from app.models2 import VideoCamera
 import keras
 
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-cap=VideoCamera()
+cap=cv2.VideoCapture('E:\\1.mp4')
 #原始图片
-image = ''
 
 @app.route('/video')
 def video():
@@ -21,17 +19,18 @@ def video():
     return render_template('video.html', title="trial")
 
 
-def gen(video):
+def gen():
     while True:
-        frame = video.get_frame()
+        ret,frame = cap.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
         # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
 
 
 @app.route('/video_play')  # 这个地址返回视频流响应
 def video_play():
-    return Response(gen(video), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/play_video/<video_address>',methods=['GET'])
 def play_video(video_address):
