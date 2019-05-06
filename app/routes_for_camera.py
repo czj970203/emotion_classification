f5bbbb1dfb7aa2ee173655fd4f9fade8219a39be
Yuiheart <151250023@smail.nju.edu.cn>
2019-05-06 15:15:13 +0800

    5/6 15:15

--- app/routes_for_camera.py
+++ app/routes_for_camera.py
@@ -57,7 +57,7 @@ def gen(camera):
             face_locations, face_encodings = cv_capture.get_key_face_info(image)
             face_num = len(face_locations)
             for i in range(face_num):
-                if True not in face_recognition.compare_faces(seen_face_encodings, face_encodings[i], tolerance=0.6):
+                if True not in face_recognition.compare_faces(seen_face_encodings, face_encodings[i], tolerance=0.5):
                     seen_face_encodings.append(face_encodings[i])
                     seen_face_num += 1
                 if seen_face_num >= 4:
@@ -109,6 +109,7 @@ def close_video():
     while not is_stopped:
         pass
     cap.__del__()
+    gc.collect()
     return render_template('index.html', is_closed=is_closed)
 
 
